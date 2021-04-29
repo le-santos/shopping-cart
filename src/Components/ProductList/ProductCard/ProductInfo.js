4db77ca0853aacd5d627e17111b5eb1fe4f11507
le-santos <lequinterio@gmail.com>
2021-04-29 00:27:05 -0300

    updates ProductInfo style

--- src/Components/ProductList/ProductCard/ProductInfo.js
+++ src/Components/ProductList/ProductCard/ProductInfo.js
@@ -8,18 +8,20 @@ const DivStyled = styled.div`
 
   h3 {
     margin-bottom: 0.7rem;
+    text-transform: capitalize;
   }
 
   p {
     color: var(--gray-1);
     margin: 0.5rem 0;
+    text-decoration: line-through;
   }
 `;
 
 export default function ProductInfo({ title, price, discountPrice }) {
   return (
     <DivStyled>
-      <h3>{title}</h3>
+      <h3>{title.toLowerCase()}</h3>
       <p>{price}</p>
       <h3>{discountPrice}</h3>
     </DivStyled>
